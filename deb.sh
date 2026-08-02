da6074b4b87926b77905ae93713a7072f10420d1
arhanth255-ui <arhanth255@gmail.com>
2026-08-02 17:02:36 +0530

    F3

--- deb.sh
+++ deb.sh
@@ -3,29 +3,39 @@ set -e
 
 VERSION="$1"
 
-cd "$(dirname "$0")/.."
-
 echo "Building Debian package..."
 
+# Clean previous build
 rm -rf deb
 
+# Create package structure
 mkdir -p deb/DEBIAN
 mkdir -p deb/usr/bin
 mkdir -p deb/usr/share/applications
 mkdir -p deb/usr/share/icons/hicolor/256x256/apps
 mkdir -p deb/usr/share/hobbyspark
 
+# Package metadata
 cp control deb/DEBIAN/control
+
+# Desktop launcher
 cp hobbyspark.desktop deb/usr/share/applications/
+
+# Launcher script
 cp hobbyspark deb/usr/bin/
-cp assets/icon.png deb/usr/share/icons/hicolor/256x256/apps/hobbyspark.png
 chmod +x deb/usr/bin/hobbyspark
 
+# Icon
+cp assets/icon.png \
+   deb/usr/share/icons/hicolor/256x256/apps/hobbyspark.png
+
+# Application files
 cp -r dist/HobbySpark/* deb/usr/share/hobbyspark/
 
+# Replace version placeholder
 sed -i "s/@VERSION@/$VERSION/g" deb/DEBIAN/control
 
-dpkg-deb --build deb
-mv deb.deb HobbySpark-${VERSION}-amd64.deb
+# Build package
+dpkg-deb --build deb "HobbySpark-${VERSION}-Linux-amd64.deb"
 
-echo "Done."
+echo "Done!"
