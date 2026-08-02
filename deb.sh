f6819eadd4f261604670b7a7aa1c096ebc011c19
arhanth255-ui <arhanth255@gmail.com>
2026-08-02 17:14:23 +0530

    F6

--- deb.sh
+++ deb.sh
@@ -3,11 +3,7 @@ set -e
 
 VERSION="$1"
 
-if [ -z "$VERSION" ]; then
-    VERSION="DEV"
-fi
-
-echo "Building Debian package..."
+echo "Building HobbySpark Debian package..."
 
 rm -rf deb
 
@@ -17,23 +13,23 @@ mkdir -p deb/usr/share/applications
 mkdir -p deb/usr/share/icons/hicolor/256x256/apps
 mkdir -p deb/usr/share/hobbyspark
 
-###############################################################################
+##############################################################################
 # control
-###############################################################################
+##############################################################################
 
 cat > deb/DEBIAN/control <<EOF
 Package: hobbyspark
 Version: $VERSION
-Section: development
+Section: devel
 Priority: optional
 Architecture: amd64
 Maintainer: HobbySpark Industries
 Description: HobbySpark Programming Language and IDE
 EOF
 
-###############################################################################
+##############################################################################
 # launcher
-###############################################################################
+##############################################################################
 
 cat > deb/usr/bin/hobbyspark <<'EOF'
 #!/bin/bash
@@ -42,41 +38,42 @@ EOF
 
 chmod +x deb/usr/bin/hobbyspark
 
-###############################################################################
-# desktop entry
-###############################################################################
+##############################################################################
+# desktop
+##############################################################################
 
 cat > deb/usr/share/applications/hobbyspark.desktop <<EOF
 [Desktop Entry]
+Version=1.0
+Type=Application
 Name=HobbySpark
 Comment=HobbySpark IDE
 Exec=hobbyspark
 Icon=hobbyspark
 Terminal=false
-Type=Application
-Categories=Development;IDE;
+Categories=Development;
 EOF
 
-###############################################################################
+##############################################################################
 # icon
-###############################################################################
+##############################################################################
 
 cp assets/icon.png \
-   deb/usr/share/icons/hicolor/256x256/apps/hobbyspark.png
+deb/usr/share/icons/hicolor/256x256/apps/hobbyspark.png
 
-###############################################################################
-# application
-###############################################################################
+##############################################################################
+# program
+##############################################################################
 
-cp -r dist/HobbySpark/* deb/usr/share/hobbyspark/
+cp -r dist/HobbySpark/* \
+deb/usr/share/hobbyspark/
 
-###############################################################################
-# build package
-###############################################################################
+##############################################################################
+# build
+##############################################################################
 
 dpkg-deb --build deb \
-    "HobbySpark-${VERSION}-Linux-amd64.deb"
+"HobbySpark-${VERSION}-Linux-amd64.deb"
 
 echo
-echo "Successfully built:"
-echo "HobbySpark-${VERSION}-Linux-amd64.deb"
+echo "Finished!"
