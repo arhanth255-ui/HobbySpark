aceb86698ecc7b3808719e0977d87a0b5564f22c
arhanth255-ui <arhanth255@gmail.com>
2026-08-02 16:03:10 +0530

    Added debian support

---
+++ deb.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+VERSION="$1"
+
+echo "Building Debian package..."
+
+rm -rf deb
+
+mkdir -p deb/DEBIAN
+mkdir -p deb/usr/bin
+mkdir -p deb/usr/share/applications
+mkdir -p deb/usr/share/icons/hicolor/256x256/apps
+mkdir -p deb/usr/share/hobbyspark
+
+cp linux/control deb/DEBIAN/control
+cp linux/hobbyspark.desktop deb/usr/share/applications/
+cp assets/icon.png deb/usr/share/icons/hicolor/256x256/apps/hobbyspark.png
+cp linux/hobbyspark deb/usr/bin/
+chmod +x deb/usr/bin/hobbyspark
+
+cp -r dist/HobbySpark/* deb/usr/share/hobbyspark/
+
+sed -i "s/@VERSION@/$VERSION/g" deb/DEBIAN/control
+
+dpkg-deb --build deb
+mv deb.deb HobbySpark-${VERSION}-amd64.deb
+
+echo "Done."
