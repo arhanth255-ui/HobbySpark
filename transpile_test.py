5aa73cb79cca6cdb10118246e83ad30ea6a650d2
arhanth255-ui <arhanth255@gmail.com>
2026-08-07 21:27:04 +0530

    F9

--- transpile_test.py
+++ transpile_test.py
@@ -1,1 +1,5 @@
 from stub import *
+set_board(ArduinoNano(), debug_mode=True)
+lcd = I2C_LCD_Display("SDA","SCL",16,2,0x28)
+lcd.scroll_with_millis("", 1, SEC)
+lcd.add("Hello world from HobbySpark")
